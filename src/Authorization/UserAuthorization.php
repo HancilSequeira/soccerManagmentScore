22e3caffe1d95d941d683191b1f86fcaabd1de9d
Hancil Sequiera <koolfriends92@gmail.com>
2020-03-30 10:48:46 +0530

    Authentication for player and team

--- src/Authorization/UserAuthorization.php
+++ src/Authorization/UserAuthorization.php
@@ -2,16 +2,42 @@
 
 namespace App\Authorization;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
-class UserAuthorization implements AccessDeniedHandlerInterface
+class UserAuthorization extends  EntityRepository implements UserProviderInterface
 {
-    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    public function __construct(EntityManagerInterface $em)
     {
-        dump($accessDeniedException);
-        die;
+        parent::__construct($em);
     }
 
+    public function loadUserByUsername($username)
+    {
+        $q = $this
+            ->createQueryBuilder('u')
+            ->select('u')
+            ->where('u.username = :username')
+            ->setParameter('username', $username)
+            ->getQuery();
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        // TODO: Implement refreshUser() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supportsClass($class)
+    {
+        // TODO: Implement supportsClass() method.
+    }
 }
